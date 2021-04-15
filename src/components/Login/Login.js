67f12c30b508c86b4745a760759e1cfc46dc5479
Hossain <hossainrabbi259@gmail.com>
2021-04-15 20:03:14 +0600

    login successfully

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import './Login.css';
 import google_icon from '../../image/google.png';
+import {
+    firebaseInitialize,
+    googleProvider,
+    signInWithSocialPopup,
+} from '../Firebase/Firebase';
+
+firebaseInitialize();
 
 const Login = () => {
+    const handleLogin = () => {
+        signInWithSocialPopup(googleProvider)
+            .then((result) => {
+                console.log(result);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
+
     return (
         <div className="login container">
             <div className="login-container">
-                <button className="btn login-btn">
+                <button className="btn login-btn" onClick={handleLogin}>
                     <img src={google_icon} alt="google_icon" />
                     <span>Sign in with Google</span>
                 </button>
